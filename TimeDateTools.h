e2e3df5582eaf7c61b1e8d5d800737c4889e65cf
Mark Cooke <mark@creativecookies.net>
2016-01-08 16:41:42 +1030

    ## TimeDateTools.h * move print_date, print_time, print_datetime from wwvb to here * rename boolean to C standard bool * bugfix: timeDateCompensate - it didnt work previously, fixed

--- TimeDateTools.h
+++ TimeDateTools.h
@@ -4,12 +4,59 @@
 #include <Arduino.h>
 #include <avr/pgmspace.h>
 
+#if !defined(_DEBUG)
+#define _DEBUG 1
+#endif
+
 //											 Jan Feb Mar Apr May Jun Jul Aug Sep Oct Nov Dec
 const uint8_t DIAM[12] PROGMEM = { 31, 28, 31, 30, 31, 30, 31, 31,30, 31, 30,  31 }; // actual (non-leap year)
 const uint16_t cumulativeDIAM[12] PROGMEM = { 0 , 31, 59, 90,120,151,181,212,243,273,304,334 }; // cumulative
 
 #define days_in_a_month(k) pgm_read_byte_near(DIAM + k)
 #define cumulative_days_in_a_month(k) pgm_read_word_near(cumulativeDIAM + k)
+
+void print_time(uint8_t _mins, uint8_t _hour, const boolean new_line = false)
+{
+#if (_DEBUG > 0)
+	if (_hour < 10) { Serial.print('0'); }
+	Serial.print(_hour);
+	Serial.print(':');
+	if (_mins < 10) { Serial.print('0'); }
+	Serial.print(_mins);
+	if (new_line)
+	{
+		Serial.println();
+	}
+#endif
+}
+
+void print_date(uint8_t _DD, uint8_t _MM, uint8_t _YY, const boolean new_line = false)
+{
+#if (_DEBUG > 0)
+	if (_DD < 10) { Serial.print('0'); }
+	Serial.print(_DD);
+	Serial.print('/');
+	if (_MM < 10) { Serial.print('0'); }
+	Serial.print(_MM);
+	Serial.print(F("/20"));
+	if (_YY < 10) { Serial.print('0'); }
+	Serial.print(_YY);
+	if (new_line)
+	{
+		Serial.println();
+	}
+#endif
+}
+
+void print_datetime(uint8_t _mins, uint8_t _hour,
+	uint8_t _DD, uint8_t _MM, uint8_t _YY)
+{
+	print_time(_mins, _hour);
+#if (_DEBUG > 0)
+	Serial.print(F(" on "));
+#endif
+	print_date(_DD, _MM, _YY, true);
+}
 
 inline uint8_t ascii_to_int(const char &c0, const char &c1)
 {
@@ -61,23 +108,23 @@ void DateString_to_DDMMYY(const char dateString[], uint8_t &DD, uint8_t &MM, uin
 }
 #endif
 
-boolean is_leap_year(const uint16_t &_year)
+bool is_leap_year(const uint16_t &_year)
 {
-	const boolean is_DivBy4 = (_year % 4) == 0;
-	const boolean is_DivBy100 = (_year % 100) == 0;
-	const boolean is_DivBy400 = (_year % 400) == 0;
+	const bool is_DivBy4 = (_year % 4) == 0;
+	const bool is_DivBy100 = (_year % 100) == 0;
+	const bool is_DivBy400 = (_year % 400) == 0;
 
 	return (is_DivBy4 & (!is_DivBy100 | is_DivBy400));
 }
 template <typename T>
-uint16_t to_day_of_the_year(const T &_DD, const T &_MM, const boolean &_is_leap_year)
+uint16_t to_day_of_the_year(const T &_DD, const T &_MM, const bool &_is_leap_year)
 {
 	// if its a leap year and the month is March -> December, add +1
 	return cumulative_days_in_a_month(_MM - 1) + _DD + (_is_leap_year & (_MM > 1));
 }
 
 template <typename T>
-void from_day_of_the_year(const uint16_t &doty, T &_DD, T &_MM, const boolean &_is_leap_year)
+void from_day_of_the_year(const uint16_t &doty, T &_DD, T &_MM, const bool &_is_leap_year)
 {
 	for (uint8_t i = 1; i < 12; ++i)
 	{
@@ -93,30 +140,34 @@ void from_day_of_the_year(const uint16_t &doty, T &_DD, T &_MM, const boolean &_
 	_MM = 0;
 }
 
-// example: input = day (unbounded), TD0 = day (bounded), TD1 = month (bounded), prevLimit = days in last month, thisLimit = days in this month
-// timeDateCompensate(-1, day, month, 28, 31)
-//
-// 1. set TD0 such that it stays within [0:thisLimit]
-// 2. set TD1 such that it stays within [0:prevLimit]
+// example: input = day (unbounded), TD0 = day (bounded), TD1 = month (bounded), prevMax = days in last month, currMax = days in this month
+// timeDateCompensate(-1, day, month, 1, 28, 31) => day = 27, month = month - 1
+// timeDateCompensate( 0, day, month, 1, 28, 31) => day = 28, month = month - 1
+// timeDateCompensate(29, day, month, 1, 28, 31) => day = 29, month is unchanged
+// timeDateCompensate(32, day, month, 1, 28, 31) => day =  1, month = month + 1
 template <typename T>
-void timeDateCompensate(const int8_t &input, T &TD0, int8_t &TD1, const uint8_t &prevLimit, const uint8_t &thisLimit)
+void timeDateCompensate(const int8_t &input, T &TD0, int8_t &TD1, const uint8_t &currMin, const uint8_t &prevMax, const uint8_t &currMax)
 {
-	if (input > (thisLimit - 1))
+	if (input > (currMax - 1 + currMin))
 	{
-		TD0 = input % thisLimit;
+		TD0 = (input % (currMax + currMin)) + currMin;
 		TD1++;
 	}
-	else if (input < 0)
+	else if (input < currMin)
 	{
-		TD0 = prevLimit - TD0;
+		TD0 = prevMax + input;
 		TD1--;
+	}
+	else
+	{
+		TD0 = input;
 	}
 }
 
 template <typename T>
-void timeDateCompensate(const int8_t &input, T &TD0, int8_t &TD1, const uint8_t &thisLimit)
+void timeDateCompensate(const int8_t &input, T &TD0, int8_t &TD1, const uint8_t &currMin, const uint8_t &currMax)
 {
-	timeDateCompensate(input, TD0, TD1, thisLimit, thisLimit);
+	timeDateCompensate(input, TD0, TD1, currMin, currMax, currMax);
 }
 
 // Note: I normally use the gps to utc seconds value (-17seconds as of Dec 2015) for _timezone_secs
@@ -129,23 +180,23 @@ void addTimezone(T &secs, T &mins, T &hour, T &DD, T &MM, T &YY, const int8_t &_
 	int8_t day_ = DD;
 	int8_t month_ = MM;
 	int8_t year_ = YY;
-
+	
 	const uint8_t prevMonth = (month_ == 1) ? 12 : month_ - 1;
 	int8_t decade_ = 20; // we dont actually do anything with this
 
-	const boolean is_leap_year_ = is_leap_year(decade_ * 100 + year_);
+	const bool is_leap_year_ = is_leap_year(decade_ * 100 + year_);
 
 	// compensate for the timezone
-	timeDateCompensate<T>(secs_, secs, mins_, 60); // secs
-	timeDateCompensate<T>(mins_, mins, hour_, 60); // mins
-	timeDateCompensate<T>(hour_, hour, day_, 60); // hour
+	timeDateCompensate<T>(secs_, secs, mins_, 0, 60); // secs
+	timeDateCompensate<T>(mins_, mins, hour_, 0, 60); // mins
+	timeDateCompensate<T>(hour_, hour, day_, 0, 24); // hour
 
 	uint8_t prevMonthDays = days_in_a_month(prevMonth - 1) + ((prevMonth == 2) & is_leap_year_); // Add 1 day if in Feb on a leapyear
 	uint8_t thisMonthDays = days_in_a_month(month_ - 1) + ((month_ == 2) & is_leap_year_); // Add 1 day if in Feb on a leapyear
-	timeDateCompensate<T>(day_, DD, month_, prevMonthDays, thisMonthDays); // day
-
-	timeDateCompensate<T>(month_, MM, year_, 12); // month
-	timeDateCompensate<T>(year_, YY, decade_, 100); // year
+	
+	timeDateCompensate<T>(day_, DD, month_, 1, prevMonthDays, thisMonthDays); // day
+	timeDateCompensate<T>(month_, MM, year_, 1, 12); // month
+	timeDateCompensate<T>(year_, YY, decade_, 0, 100); // year
 }
 
 #endif
