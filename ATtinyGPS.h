5d6aa0cd52c133cce3397717beabcaff3a5e885e
Mark Cooke <mark@creativecookies.net>
2016-02-04 17:37:22 +1030

    ## ATtinyGPS.h * declared msg[] as static

--- ATtinyGPS.h
+++ ATtinyGPS.h
@@ -4,10 +4,10 @@
 #include <TimeDateTools.h>
 
 /*
- Examples:
+Examples:
 token:   0    1     2    3      4    5    6     7     8      9 10 11
 $GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
-      RMC          Recommended Minimum sentence C
+RMC          Recommended Minimum sentence C
 0     123519       Fix taken at 12:35:19 UTC
 1     A            Status A=active or V=Void.
 2,3   4807.038,N   Latitude 48 deg 07.038' N
@@ -17,28 +17,28 @@ $GPRMC,123519,A,4807.038,N,01131.000,E,022.4,084.4,230394,003.1,W*6A
 8     230394       Date - 23rd of March 1994
 9,10  003.1,W      Magnetic Variation
 11    *6A          The checksum data, always begins with *
- 
+
 token:    0       1    2      3    4 5  6  7     8  9  10  11  12
 $GPGGA,123519,4807.038,N,01131.000,E,1,08,0.9,545.4,M,46.9,M,,*47
-      GGA          Global Positioning System Fix Data
+GGA          Global Positioning System Fix Data
 0     123519       Fix taken at 12:35:19 UTC
 1,2   4807.038,N   Latitude 48 deg 07.038' N
 3,4   01131.000,E  Longitude 11 deg 31.000' E
 5     1            Fix quality: 0 = invalid
-      1 = GPS fix (SPS)
-      2 = DGPS fix
-      3 = PPS fix
-      4 = Real Time Kinematic
-      5 = Float RTK
-      6 = estimated (dead reckoning) (2.3 feature)
-      7 = Manual input mode
-      8 = Simulation mode
+1 = GPS fix (SPS)
+2 = DGPS fix
+3 = PPS fix
+4 = Real Time Kinematic
+5 = Float RTK
+6 = estimated (dead reckoning) (2.3 feature)
+7 = Manual input mode
+8 = Simulation mode
 6     08           Number of satellites being tracked
 7     0.9          Horizontal dilution of position
 8,9   545.4,M      Altitude, Meters, above mean sea level
 10,11 46.9,M       Height of geoid (mean sea level) above WGS84 ellipsoid
-      (empty field) time in seconds since last DGPS update
-      (empty field) DGPS station ID number
+(empty field) time in seconds since last DGPS update
+(empty field) DGPS station ID number
 12    *47          the checksum data, always begins with *
 */
 
@@ -66,37 +66,37 @@ case 18: break; // ZDA
 case 19: break; // MCHN
 */
 
-char msg[7] = "$$$$$$";
+static char msg[7] = "$$$$$$";
 
 class ATtinyGPS
 {
-private:
-	int state_ = 0;
-	boolean new_data_ = false;
-	
-	int32_t lhs_ = 0;
-	uint16_t rhs_ = 0;
-	uint8_t dp_ = 0, token_ = 0;
-	boolean LR_switch = false;
-   int8_t timezone_MM, timezone_HH;   
-public:
-	uint8_t nmea_index = 0;
+   private:
+   int state_ = 0;
+   boolean new_data_ = false;
+   
+   int32_t lhs_ = 0;
+   uint16_t rhs_ = 0;
+   uint8_t dp_ = 0, token_ = 0;
+   boolean LR_switch = false;
+   int8_t timezone_MM, timezone_HH;
+   public:
+   uint8_t nmea_index = 0;
 
-	// GPS variables
-	uint32_t Time, Date, Course;
-	uint8_t hh, mm, ss, ms;
-	uint8_t DD, MM, YY;
-	uint16_t YYYY;
+   // GPS variables
+   uint32_t Time, Date, Course;
+   uint8_t hh, mm, ss, ms;
+   uint8_t DD, MM, YY;
+   uint16_t YYYY;
 
-	int8_t GPS_to_UTC_offset; // -17 (seconds - as of 1/1/16)
+   int8_t GPS_to_UTC_offset; // -17 (seconds - as of 1/1/16)
 
-	float Lat, Long, Alt, Height, Knots;
-	
-	uint8_t satellites = 0;
-	uint8_t quality = 0;
-	boolean IsValid = false;
-
-	ATtinyGPS() : timezone_HH(0), timezone_MM(0), GPS_to_UTC_offset(0), IsValid(false) {};
+   float Lat, Long, Alt, Height, Knots;
+   
+   uint8_t satellites = 0;
+   uint8_t quality = 0;
+   boolean IsValid = false;
+   
+   ATtinyGPS() : timezone_HH(0), timezone_MM(0), GPS_to_UTC_offset(0), IsValid(false){};
    
    void setTimezone(const int8_t &_hour, const int8_t &_mins)
    {
@@ -107,180 +107,180 @@ public:
    {
       _hour = timezone_HH;
       _mins = timezone_MM;
-   } 
-	void parse(char c)
-	{
-		if (c == '$')
-		{
-			lhs_ = 0; rhs_ = 0; LR_switch = false;
-			token_ = 0; state_ = 0; nmea_index = 0;
-			msg[5] = '?'; new_data_ = false;
-		}
-		
-		// Start recording the NMEA message type
-		if (state_ < 6)
-		{
-			msg[state_] = c;
-			state_++;
-			return;
-		}
-		// Set the NMEA index
-		else if (state_ == 6)
-		{
-			if (strcmp(msg, "$GPRMC") == 0) { nmea_index = 1; }
-			else if (strcmp(msg, "$GPGGA") == 0) { nmea_index = 4; }
-			else
+   }
+   void parse(char c)
+   {
+      if (c == '$')
+      {
+         lhs_ = 0; rhs_ = 0; LR_switch = false;
+         token_ = 0; state_ = 0; nmea_index = 0;
+         msg[5] = '?'; new_data_ = false;
+      }
+      
+      // Start recording the NMEA message type
+      if (state_ < 6)
+      {
+         msg[state_] = c;
+         state_++;
+         return;
+      }
+      // Set the NMEA index
+      else if (state_ == 6)
+      {
+         if (strcmp(msg, "$GPRMC") == 0) { nmea_index = 1; }
+         else if (strcmp(msg, "$GPGGA") == 0) { nmea_index = 4; }
+         else
          {
             #if (_DEBUG > 0)
             Serial.print("error : NMEA string "); Serial.print(msg); Serial.println(" is not supported");
-			#endif
+            #endif
             nmea_index = 0;
          }
 
-			state_++;
-		}
-		// Parse the data
-		else if ( nmea_index > 0 )
-		{
-			switch (c)
-			{
-				// Validity indicators:
-				// 
-				// RMC token 1
-				// A=*A-active, V-void
-				//
-				// RMC token 11
-				// GLL token 6
-				// *A-autonomous, *D-differential, E-Estimated, N-not valid, S-Simulator
-				//
-				// GGA token 5
-				// 0 = invalid, GPS fix, DGPS fix, PPS fix, Real Time Kinematic (RTK), Float RTK, estimated (dead reckoning), Manual input, 8 = Simulation
-			case 'A':
-				// RMC: token 1
-				quality = 1;
-				IsValid = true; break;
-			case 'V':
-				// RMC: token 1
-				quality = 0;
-				IsValid = false; break;
-			case 'N':
-				// N/S(-ve) - RMC: token 3
-				// N/S(-ve) - GGA: token 2
-				if ( (token_ == 3) | (token_ == 2) ) { Lat = -Lat; } break;
-			case 'E':
-				// E/W(-ve) - RMC: token 5
-				// E/W(-ve) - GGA: token 4
-				if ( (token_ == 5) | (token_ == 4) ) { Long = -Long; } break;
-			case '.':
-				LR_switch = true;
-				break;
-			case ',':
-				saveToken();
-				token_++;
-				LR_switch = false;
-				break;
-			case '*':
-				nmea_index = 0;
-				new_data_ = true;
-				saveToken();
-				// may have to change this in 85 years...
-				YYYY = 2000 + YY;
-				// Add the local timezone to the GPS time
-				// And add the GPS to UTC offset
-				// (GPS doesn't compensate for leap seconds, as of Dec 2015 its 17 seconds ahead of UTC)
-				addTimezone<uint8_t>(ss, mm, hh, DD, MM, YY, timezone_HH, timezone_MM, GPS_to_UTC_offset);
-				break;
-			default:
-				char d = c - '0';
-				if (LR_switch) // Right
-				{
-					rhs_ = rhs_ * 10 + d;
-					dp_++;
-				}
-				else // Left
-				{
-					lhs_ = lhs_ * 10 + d;
-				}
-				break;
-			}
-		}
-	}
+         state_++;
+      }
+      // Parse the data
+      else if ( nmea_index > 0 )
+      {
+         switch (c)
+         {
+            // Validity indicators:
+            //
+            // RMC token 1
+            // A=*A-active, V-void
+            //
+            // RMC token 11
+            // GLL token 6
+            // *A-autonomous, *D-differential, E-Estimated, N-not valid, S-Simulator
+            //
+            // GGA token 5
+            // 0 = invalid, GPS fix, DGPS fix, PPS fix, Real Time Kinematic (RTK), Float RTK, estimated (dead reckoning), Manual input, 8 = Simulation
+            case 'A':
+            // RMC: token 1
+            quality = 1;
+            IsValid = true; break;
+            case 'V':
+            // RMC: token 1
+            quality = 0;
+            IsValid = false; break;
+            case 'N':
+            // N/S(-ve) - RMC: token 3
+            // N/S(-ve) - GGA: token 2
+            if ( (token_ == 3) | (token_ == 2) ) { Lat = -Lat; } break;
+            case 'E':
+            // E/W(-ve) - RMC: token 5
+            // E/W(-ve) - GGA: token 4
+            if ( (token_ == 5) | (token_ == 4) ) { Long = -Long; } break;
+            case '.':
+            LR_switch = true;
+            break;
+            case ',':
+            saveToken();
+            token_++;
+            LR_switch = false;
+            break;
+            case '*':
+            nmea_index = 0;
+            new_data_ = true;
+            saveToken();
+            // may have to change this in 85 years...
+            YYYY = 2000 + YY;
+            // Add the local timezone to the GPS time
+            // And add the GPS to UTC offset
+            // (GPS doesn't compensate for leap seconds, as of Dec 2015 its 17 seconds ahead of UTC)
+            addTimezone<uint8_t>(ss, mm, hh, DD, MM, YY, timezone_HH, timezone_MM, GPS_to_UTC_offset);
+            break;
+            default:
+            char d = c - '0';
+            if (LR_switch) // Right
+            {
+               rhs_ = rhs_ * 10 + d;
+               dp_++;
+            }
+            else // Left
+            {
+               lhs_ = lhs_ * 10 + d;
+            }
+            break;
+         }
+      }
+   }
 
-	boolean new_data()
-	{
-		if (new_data_)
-		{
-			new_data_ = false;
-			return true;
-		}
-		return false;
-	}
-private:
-	void saveToken()
-	{
-		switch (token_)
-		{
-		case 0: // Time: hhmmss.ms
-			Time = lhs_;
-			ms = rhs_;
-			ss = Time % 100; Time /= 100;
-			mm = Time % 100; Time /= 100;
-			hh = Time % 100; Time = lhs_;
+   boolean new_data()
+   {
+      if (new_data_)
+      {
+         new_data_ = false;
+         return true;
+      }
+      return false;
+   }
+   private:
+   void saveToken()
+   {
+      switch (token_)
+      {
+         case 0: // Time: hhmmss.ms
+         Time = lhs_;
+         ms = rhs_;
+         ss = Time % 100; Time /= 100;
+         mm = Time % 100; Time /= 100;
+         hh = Time % 100; Time = lhs_;
          #if (_DEBUG == 2)
          Serial.println();
          Serial.print("RAW TIME : "); Serial.println(lhs_);
          Serial.print("PROCESSED hh:mm:ss : "); Serial.print(hh); Serial.print(":"); Serial.print(mm); Serial.print(":");Serial.println(ss);
          #endif
-			break;
-		case 1:
-			if (nmea_index == 4) { Lat = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			break;
-		case 2:
-			if (nmea_index == 1) { Lat = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			break;
-		case 3:
-			if (nmea_index == 4) { Long = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			break;
-		case 4:
-			if (nmea_index == 1) { Long = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			break;
-		case 5:
-			if (nmea_index == 4) { quality = lhs_; }
-			break;
-		case 6:
-			if (nmea_index == 1) { Knots = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			if (nmea_index == 4) { satellites = lhs_; }
-			break;
-		case 7:
-			//  1 : RMC -> Track angle in degrees true
-			break;
-		case 8:
-			if (nmea_index == 1)
-			{ 
-				//  1 : RMC -> Date: DDMMYY
-				Date = lhs_;
-				YY = Date % 100; Date /= 100;
-				MM = Date % 100; Date /= 100;
-				DD = Date % 100; Date = lhs_;
+         break;
+         case 1:
+         if (nmea_index == 4) { Lat = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         break;
+         case 2:
+         if (nmea_index == 1) { Lat = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         break;
+         case 3:
+         if (nmea_index == 4) { Long = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         break;
+         case 4:
+         if (nmea_index == 1) { Long = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         break;
+         case 5:
+         if (nmea_index == 4) { quality = lhs_; }
+         break;
+         case 6:
+         if (nmea_index == 1) { Knots = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         if (nmea_index == 4) { satellites = lhs_; }
+         break;
+         case 7:
+         //  1 : RMC -> Track angle in degrees true
+         break;
+         case 8:
+         if (nmea_index == 1)
+         {
+            //  1 : RMC -> Date: DDMMYY
+            Date = lhs_;
+            YY = Date % 100; Date /= 100;
+            MM = Date % 100; Date /= 100;
+            DD = Date % 100; Date = lhs_;
             #if (_DEBUG == 2)
             Serial.println();
             Serial.print("RAW DATE : "); Serial.println(lhs_);
             Serial.print("PROCESSED DD/MM/YY: "); Serial.print(DD); Serial.print("/"); Serial.print(MM); Serial.print("/"); Serial.println(YY);
             #endif
-			}
-			if (nmea_index == 4) {  Alt = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			break;
-		case 9:
-			break;
-		case 10:
-			if (nmea_index == 4) {  Height = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
-			break;
-		}
+         }
+         if (nmea_index == 4) {  Alt = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         break;
+         case 9:
+         break;
+         case 10:
+         if (nmea_index == 4) {  Height = lhs_ + static_cast<float>(rhs_) / pow(10.0, dp_); }
+         break;
+      }
 
-		// clear temp variables
-		lhs_ = 0;
-		rhs_ = 0;
-	}
+      // clear temp variables
+      lhs_ = 0;
+      rhs_ = 0;
+   }
 };
 
 #endif
